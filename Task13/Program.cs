c7a0ac8fb10a99784ebbb79d038e6faeda3abbc3
Alex <akhusyainov@gmail.com>
2022-09-06 22:53:16 +0300

    сделал задачи 10,13,15

---
+++ Task13/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Clear();
+Console.Write("Пользователь, введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 99)
+Console.WriteLine("Третьей цифры нет");
+while (num >= 999)
+{
+    num = num / 10;
+}
+int win;
+win = num % 10;
+Console.WriteLine(win);
+
+
+
+
+
+// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// int number = new Random().Next(100, 1000);
+// int firstDigit = number / 100; 
+// int secondDigit = number % 10;
+// Console.WriteLine($"число {number} без средней цифры -> {firstDigit}, {secondDigit}");
+
+// int number = new Random().Next(100, 1000);
+
+// int MaxDigit(int num)
+// {
+//     int firstDigit = num / 100 * 10;
+//     int secondDigit = num % 10;    
+//     return firstDigit + secondDigit;
+// }
+// int result = MaxDigit(number);
+// Console.WriteLine($"{number} -> {result}");
+
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
---
+++ Task15/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+// Console.Clear();
+// Console.Write("Пользователь, введите число, соответствующее дню недели и ты узнаешь, выходной ли этот день: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num == 1)
+// {
+// Console.WriteLine($"нет");
+// }
+// if (num == 2)
+// {
+// Console.WriteLine($"нет");
+// }
+// if (num == 3)
+// {
+// Console.WriteLine($"нет");
+// }
+// if (num == 4)
+// {
+// Console.WriteLine($"нет");
+// }
+// if (num == 5)
+// {
+// Console.WriteLine($"нет");
+// }
+// if (num == 6)
+// {
+// Console.WriteLine($"да");
+// }
+// if (num == 7)
+// {
+// Console.WriteLine($"да");
+// }
+// if (num > 7)
+// Console.WriteLine("Пользователь, в неделе 7 дней");
+
+Console.Clear();
+Console.Write("Пользователь, введите число, соответствующее дню недели и ты узнаешь, выходной ли этот день: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 5)
+{
+Console.WriteLine($"нет");
+}
+if (num > 5 && num <= 7)
+{
+Console.WriteLine($"да");
+}
+if (num > 7)
+Console.WriteLine("Пользователь,увы, в неделе 7 дней");
